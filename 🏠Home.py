ed075d34b17bc1f26f5b58083a0c31ced85b6e5d
GurjodhSandhu <114323787+GurjodhSandhu@users.noreply.github.com>
2024-04-08 20:16:25 -0700

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -174,7 +174,7 @@ if st.session_state["authentication_status"]: #if the user is authenticated curr
 
 else: #registration for the website
     try:
-        email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(preauthorization=False)
+        email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
         if email_of_registered_user:
             st.success('User registered successfully')
             config['credentials']['usernames'][username_of_registered_user]['points'] = 20
